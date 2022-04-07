af492c68a2d9888ae43b983997b935651bedbdfb
zeapoz <zeapo@pm.me>
2022-04-07 13:42:59 +0200

    Added basic sorting

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -19,11 +19,26 @@ namespace SchoolWebApp.Pages.Students
             _context = context;
         }
 
+        public string NameSort { get; set; }
+
         public IList<Student> Student { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students select s;
+
+            switch (sortOrder) {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Student = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
